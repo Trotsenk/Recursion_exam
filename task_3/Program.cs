a965b9c257cab0ed732bf330b7b313480809b2cb
Ivan Trotsenko <trotsenkoinan@mail.ru>
2024-07-29 14:46:37 +0300

    Task 3 done

---
+++ task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+class Program
+{
+   static void Main(string[] args)
+   {
+       int[] array = { 1, 2, 5, 10, 30 };
+       PrintArrayReversed(array, array.Length - 1);
+   }
+   static void PrintArrayReversed(int[] array, int index)
+   {
+       if (index >= 0)
+       {
+           Console.Write(array[index] + " ");
+           PrintArrayReversed(array, index - 1);
+       }
+   }
+}
